c1b66ece260c00f52a9933cdd4e2c2f827cdea0a
Matthew Frisby <frisbm@gmail.com>
2023-06-17 13:50:28 -0500

    create cron for checking schedule

--- cron.go
+++ cron.go
@@ -1,6 +1,9 @@
 package cron
 
-import "time"
+import (
+	"golang.org/x/exp/slices"
+	"time"
+)
 
 type Cron struct {
 	minute    []uint8
@@ -12,5 +15,94 @@ type Cron struct {
 }
 
 func (c *Cron) UseLocal() {
-	c.location = time.Local
+	c.utc = false
+}
+
+func (c *Cron) NextFrom(from time.Time) time.Time {
+	if c.utc {
+		from = from.UTC()
+	}
+	nextTime := from.Add(time.Minute)
+
+	for {
+		if slices.Contains(c.minute, uint8(nextTime.Minute())) &&
+			slices.Contains(c.hour, uint8(nextTime.Hour())) &&
+			slices.Contains(c.day, uint8(nextTime.Day())) &&
+			slices.Contains(c.month, uint8(nextTime.Month())) &&
+			slices.Contains(c.dayOfWeek, uint8(nextTime.Weekday())) {
+			break
+		}
+
+		nextTime = nextTime.Add(time.Minute)
+	}
+
+	return nextTime
+}
+
+func (c *Cron) Next() time.Time {
+	return c.NextFrom(c.now())
+}
+
+func (c *Cron) Prev() time.Time {
+	return c.PrevBefore(c.now())
+}
+
+func (c *Cron) PrevBefore(before time.Time) time.Time {
+	if c.utc {
+		before = before.UTC()
+	}
+	prevTime := before.Add(-1 * time.Minute)
+
+	for {
+		if slices.Contains(c.minute, uint8(prevTime.Minute())) &&
+			slices.Contains(c.hour, uint8(prevTime.Hour())) &&
+			slices.Contains(c.day, uint8(prevTime.Day())) &&
+			slices.Contains(c.month, uint8(prevTime.Month())) &&
+			slices.Contains(c.dayOfWeek, uint8(prevTime.Weekday())) {
+			break
+		}
+
+		prevTime = prevTime.Add(-1 * time.Minute)
+	}
+
+	return prevTime
+}
+
+func (c *Cron) Now() bool {
+	now := c.now()
+	if c.utc {
+		now = now.UTC()
+	}
+
+	if !slices.Contains(c.minute, uint8(now.Minute())) {
+		return false
+	}
+
+	if !slices.Contains(c.hour, uint8(now.Hour())) {
+		return false
+	}
+
+	if !slices.Contains(c.day, uint8(now.Day())) {
+		return false
+	}
+
+	if !slices.Contains(c.month, uint8(now.Month())) {
+		return false
+	}
+
+	if !slices.Contains(c.dayOfWeek, uint8(now.Weekday())) {
+		return false
+	}
+
+	return true
+}
+
+var timeNow = time.Now
+
+func (c *Cron) now() time.Time {
+	now := timeNow().Truncate(1 * time.Minute)
+	if c.utc {
+		now = now.UTC()
+	}
+	return now
 }
---
+++ cron_test.go
@@ -0,0 +1,184 @@
+package cron
+
+import (
+	"github.com/stretchr/testify/assert"
+	"testing"
+	"time"
+)
+
+func TestCron_Next(t *testing.T) {
+	tests := []struct {
+		name     string
+		schedule string
+		now      time.Time
+		want     time.Time
+	}{
+		{
+			name:     "next min",
+			schedule: "* * * * *",
+			want:     time.Date(2023, 6, 17, 18, 24, 0, 0, time.UTC),
+		},
+		{
+			name:     "next 5th min",
+			schedule: "*/5 * * * *",
+			want:     time.Date(2023, 6, 17, 18, 25, 0, 0, time.UTC),
+		},
+		{
+			name:     "next top of the hour",
+			schedule: "0 * * * *",
+			want:     time.Date(2023, 6, 17, 19, 0, 0, 0, time.UTC),
+		},
+		{
+			name:     "next day at midnight",
+			schedule: "0 0 * * *",
+			want:     time.Date(2023, 6, 18, 0, 0, 0, 0, time.UTC),
+		},
+		{
+			name:     "next month on first day at midnight",
+			schedule: "0 0 1 * *",
+			want:     time.Date(2023, 7, 1, 0, 0, 0, 0, time.UTC),
+		},
+		{
+			name:     "First day of the next year",
+			schedule: "0 0 1 1 *",
+			want:     time.Date(2024, 1, 1, 0, 0, 0, 0, time.UTC),
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			timeNow = func() time.Time {
+				return time.Date(2023, 6, 17, 18, 23, 0, 0, time.UTC)
+			}
+			cron, err := Parse(tt.schedule)
+			assert.NoError(t, err)
+			next := cron.Next()
+			assert.True(t, tt.want.Equal(next))
+		})
+	}
+}
+
+func TestCron_Prev(t *testing.T) {
+	tests := []struct {
+		name     string
+		schedule string
+		now      time.Time
+		want     time.Time
+	}{
+		{
+			name:     "prev min",
+			schedule: "* * * * *",
+			want:     time.Date(2023, 6, 17, 18, 22, 0, 0, time.UTC),
+		},
+		{
+			name:     "prev 5th min",
+			schedule: "*/5 * * * *",
+			want:     time.Date(2023, 6, 17, 18, 20, 0, 0, time.UTC),
+		},
+		{
+			name:     "prev top of the hour",
+			schedule: "0 * * * *",
+			want:     time.Date(2023, 6, 17, 18, 0, 0, 0, time.UTC),
+		},
+		{
+			name:     "prev day at midnight",
+			schedule: "0 0 * * *",
+			want:     time.Date(2023, 6, 17, 0, 0, 0, 0, time.UTC),
+		},
+		{
+			name:     "prev month on first day at midnight",
+			schedule: "0 0 1 * *",
+			want:     time.Date(2023, 6, 1, 0, 0, 0, 0, time.UTC),
+		},
+		{
+			name:     "First day of the prev year",
+			schedule: "0 0 1 1 *",
+			want:     time.Date(2023, 1, 1, 0, 0, 0, 0, time.UTC),
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			timeNow = func() time.Time {
+				return time.Date(2023, 6, 17, 18, 23, 0, 0, time.UTC)
+			}
+			cron, err := Parse(tt.schedule)
+			assert.NoError(t, err)
+			prev := cron.Prev()
+			assert.True(t, tt.want.Equal(prev))
+		})
+	}
+}
+
+func TestCron_Now(t *testing.T) {
+	tests := []struct {
+		name     string
+		schedule string
+		now      time.Time
+		want     bool
+	}{
+		{
+			name:     "is now",
+			schedule: "* * * * *",
+			want:     true,
+		},
+		{
+			name:     "is not now - min",
+			schedule: "5 * * * *",
+			want:     false,
+		},
+		{
+			name:     "is not now - hour",
+			schedule: "* 5 * * *",
+			want:     false,
+		},
+		{
+			name:     "is not now - day",
+			schedule: "* * 5 * *",
+			want:     false,
+		},
+		{
+			name:     "is not now - month",
+			schedule: "* * * 5 *",
+			want:     false,
+		},
+		{
+			name:     "is not now - day of the week",
+			schedule: "* * * * 5",
+			want:     false,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			timeNow = func() time.Time {
+				return time.Date(2023, 6, 17, 18, 23, 0, 0, time.UTC)
+			}
+			cron, err := Parse(tt.schedule)
+			assert.NoError(t, err)
+			now := cron.Now()
+			assert.Equal(t, tt.want, now)
+		})
+	}
+}
+
+func TestCron_UseLocal(t *testing.T) {
+	tests := []struct {
+		name     string
+		schedule string
+	}{
+		{
+			name:     "use local",
+			schedule: "* * * * *",
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			timeNow = func() time.Time {
+				return time.Now().Local()
+			}
+			cron, err := Parse(tt.schedule)
+			assert.NoError(t, err)
+			cron.UseLocal()
+			assert.Equal(t, timeNow().Location(), cron.now().Location())
+			assert.True(t, timeNow().Truncate(1*time.Minute).Equal(cron.now()))
+		})
+	}
+}
